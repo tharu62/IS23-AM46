bc1634e7c696c32f34372057d03a74c205d94a24
tharu62 <deshantharindu.edirisinghe@mail.polimi.it>
2023-03-28 15:46:19 +0200

    Initial commit

--- src/main/java/it/polimi/ingsw/MODEL/BOOKSHELF.java
+++ src/main/java/it/polimi/ingsw/MODEL/BOOKSHELF.java
@@ -4,6 +4,7 @@ public class BOOKSHELF {
     item[][] Grid= new item[6][5];
     boolean IsFull=false;
     item[] itemToPut= new item[3];
+    int a;
 
     public void putItems(int m,int a, int b, int c){
         Grid[a][m]=itemToPut[0];
