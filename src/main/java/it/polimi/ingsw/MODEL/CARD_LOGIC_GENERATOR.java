2aee4a7dd9580afd9e6c5d56509d7df153e4f3e7
tharu62 <deshantharindu.edirisinghe@mail.polimi.it>
2023-04-01 18:14:15 +0200

    Initial commit

--- src/main/java/it/polimi/ingsw/MODEL/CARD_LOGIC_GENERATOR.java
+++ src/main/java/it/polimi/ingsw/MODEL/CARD_LOGIC_GENERATOR.java
@@ -8,14 +8,55 @@ public class CARD_LOGIC_GENERATOR {
     int upperbound = 25;
     int i = rand.nextInt(upperbound);
     public CARD_LOGIC SetCardLogic() {
-        if (i < 10) {
+        if (i==1) {
             this.temp = new CARD_LOGIC_1();
             return temp;
         }
-        if (i > 10) {
+        if (i==2) {
             this.temp = new CARD_LOGIC_2();
             return temp;
         }
+        if (i==3) {
+            this.temp = new CARD_LOGIC_3();
+            return temp;
+        }
+        if (i==4) {
+            this.temp = new CARD_LOGIC_4();
+            return temp;
+        }
+        if (i==5) {
+            this.temp = new CARD_LOGIC_5();
+            return temp;
+        }
+        if (i==6) {
+            this.temp = new CARD_LOGIC_6();
+            return temp;
+        }
+        if (i==7) {
+            this.temp = new CARD_LOGIC_7();
+            return temp;
+        }
+        if (i==8) {
+            this.temp = new CARD_LOGIC_8();
+            return temp;
+        }
+        if (i==9) {
+            this.temp = new CARD_LOGIC_9();
+            return temp;
+        }
+        /**
+        if (i==10) {
+            this.temp = new CARD_LOGIC_10();
+            return temp;
+        }if (i==11) {
+            this.temp = new CARD_LOGIC_11();
+            return temp;
+        }if (i==12) {
+            this.temp = new CARD_LOGIC_12();
+            return temp;
+        }
+         **/
+
         return new CARD_LOGIC_1();
     }
 }
