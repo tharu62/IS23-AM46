7178d112a36454e1378e199ad0965d2929643913
LorenzoCodega <lorenzo.codega@mail.polimi.it>
2023-04-02 20:56:16 +0200

    Added the classes from CARD_LOGIC_10 to CARD_LOGIC_12

--- src/main/java/it/polimi/ingsw/MODEL/CARD_LOGIC.java
+++ src/main/java/it/polimi/ingsw/MODEL/CARD_LOGIC.java
@@ -1,11 +1,12 @@
 package it.polimi.ingsw.MODEL;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public interface CARD_LOGIC {
     public boolean CheckCardLogic(BOOKSHELF bookshelf);
-
 }
 
 class CARD_LOGIC_1 implements CARD_LOGIC{
@@ -225,5 +226,130 @@ class CARD_LOGIC_9 implements CARD_LOGIC {
             }
         }
         return cont == 2;
+    }
+}
+
+class CARD_LOGIC_10 implements CARD_LOGIC {
+    /**
+     * This method return true if inside the player's bookshelf there are
+     * five columns of increasing or decreasing height. Starting from the first column on the left or on the right,
+     * each next column must be made of exactly one more tile. Tiles can be of any type.
+     * @param bookshelf: player's bookshelf
+     */
+    public boolean CheckCardLogic(BOOKSHELF bookshelf) {
+        item[][] grid = bookshelf.getGrid();
+        int[] cont = {0, 0, 0, 0};
+        for (int i = 0; i < 5; i++) {
+            if (grid[i][i].equals(item.EMPTY) && !grid[i + 1][i].equals(item.EMPTY)) cont[0]++;
+            if (grid[i][4 - i].equals(item.EMPTY) && !grid[i + 1][4 - i].equals(item.EMPTY)) cont[1]++;
+            if (i > 0) {
+                if (!grid[i][i].equals(item.EMPTY) && grid[i - 1][i].equals(item.EMPTY)) cont[2]++;
+                if (!grid[i][4 - i].equals(item.EMPTY) && grid[i - 1][4 - i].equals(item.EMPTY)) cont[3]++;
+            }
+        }
+        if (!grid[0][0].equals(item.EMPTY)) cont[2]++;
+        if (!grid[0][4].equals(item.EMPTY)) cont[3]++;
+        for (int i = 0; i < 4; i++) {
+            if (cont[i] == 5) return true;
+        }
+        return false;
+    }
+}
+
+class CARD_LOGIC_11 implements CARD_LOGIC {
+    /**
+     * This method return true if inside the player's bookshelf there are
+     * six groups each containing at least 2 tiles of the same type.
+     * The tiles of one group can be different from those of another group
+     * @param bookshelf: player's bookshelf
+     */
+    public boolean CheckCardLogic(BOOKSHELF bookshelf) {
+        int cont = 0;
+        item[][] grid = bookshelf.getGrid();
+        List<item> itemToCheck = new ArrayList<item>();
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 5; j++) {
+                item tile = grid[i][j];
+                if (!tile.equals(item.EMPTY) && !itemToCheck.contains(tile)) {
+                    if ((tile.equals(grid[i][j + 1]) || tile.equals(grid[i + 1][j])) && j < 4 && i < 5) {
+                        cont++;
+                        if (tile.equals(grid[i][j + 1])) itemToCheck.add(grid[i][j + 1]);
+                        if (tile.equals(grid[i + 1][j])) itemToCheck.add(grid[i + 1][j]);
+                        if (tile.equals(grid[i][j - 1])) itemToCheck.add(grid[i][j - 1]);
+                    }
+                    if (j == 4 && i < 5)
+                        if (tile.equals(grid[i + 1][j])) {
+                            cont++;
+                            itemToCheck.add(grid[i + 1][j]);
+                        }
+                    if (i == 5 && j < 4)
+                        if (tile.equals(grid[i][j + 1])){
+                            cont++;
+                            itemToCheck.add(grid[i][j + 1]);
+                        }
+                } else if (itemToCheck.contains(tile)) {
+                    if (tile.equals(grid[i][j + 1])) itemToCheck.add(grid[i][j + 1]);
+                    if (tile.equals(grid[i + 1][j])) itemToCheck.add(grid[i + 1][j]);
+                    itemToCheck.remove(tile);
+                }
+            }
+        }
+        return cont == 6;
+    }
+}
+
+class CARD_LOGIC_12 implements CARD_LOGIC {
+    /**
+     * This method return true if inside the player's bookshelf there are
+     * four groups each containing at least 4 tiles of the same type.
+     * The tiles of one group can be different from those of another group.
+     * @param bookshelf: player's bookshelf
+     */
+    public boolean CheckCardLogic(BOOKSHELF bookshelf) {
+        int cont = 0, cont_item = 0;
+        item[][] grid = bookshelf.getGrid();
+        item tile;
+        List<item> itemToCheck = new ArrayList<item>();
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 5; j++) {
+                tile = grid[i][j];
+                if (!tile.equals(item.EMPTY) && !itemToCheck.contains(tile)) {
+                    cont_item = countNeighboursType(grid, i, j, null) + 1;
+                    if (j < 4 && tile.equals(grid[i][j + 1])) itemToCheck.add(grid[i][j + 1]);
+                    if (i < 5 && tile.equals(grid[i + 1][j])) itemToCheck.add(grid[i + 1][j]);
+                    if (j > 0 && tile.equals(grid[i][j - 1])) itemToCheck.add(grid[i][j - 1]);
+                } else if (itemToCheck.contains(tile)) {
+                    if (j < 4 && tile.equals(grid[i][j + 1])) itemToCheck.add(grid[i][j + 1]);
+                    if (i < 5 && tile.equals(grid[i + 1][j])) itemToCheck.add(grid[i + 1][j]);
+                    itemToCheck.remove(tile);
+                }
+                if (cont_item > 3) cont++;
+            }
+        }
+        return cont == 4;
+    }
+
+    /**
+     * This method count how many tiles of the same type are nearby
+     * @param table: it's the bookshelf
+     * @param row: the row where the tile is located
+     * @param column: the column where the tile is located
+     * @param list: a list containing the tiles that have already been checked
+     * @return the number of tiles of the same type that are nearby
+     */
+    private int countNeighboursType(item[][] table, int row, int column, List<item> list) {
+        item tile = table[row][column];
+        int cont = 0;
+        list.add(tile);
+        if (column < 4 && tile.equals(table[row][column + 1]) && !list.contains(table[row][column + 1])) {
+            cont = 1 + countNeighboursType(table, row, column + 1, list);
+        }
+        if (row < 5 && tile.equals(table[row + 1][column]) && !list.contains(table[row + 1][column])) {
+            cont = 1 + countNeighboursType(table, row + 1, column, list);
+        }
+        if (column > 0 && tile.equals(table[row][column - 1]) && !list.contains(table[row][column - 1])) {
+            cont = 1 + countNeighboursType(table, row, column - 1, list);
+        }
+        return cont;
     }
 }
