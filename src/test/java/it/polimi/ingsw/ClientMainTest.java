c3bcf427f48c96e2228312396b47e7b4ae97d7a5
tharu62 <deshantharindu.edirisinghe@mail.polimi.it>
2023-04-04 18:44:28 +0200

    Aggiungo nuovi i metodi ServerMain e ClientMain.

---
+++ src/test/java/it/polimi/ingsw/ClientMainTest.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw;
+
+import junit.framework.TestCase;
+
+public class ClientMainTest extends TestCase {
+
+}
---
+++ src/test/java/it/polimi/ingsw/ServerMainTest.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw;
+
+import junit.framework.TestCase;
+
+public class ServerMainTest extends TestCase {
+
+}
