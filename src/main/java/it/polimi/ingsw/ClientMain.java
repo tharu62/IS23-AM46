6b8bec23156adc9d6f1dd6c3d1dbaf194cbd1426
tharu62 <deshantharindu.edirisinghe@mail.polimi.it>
2023-04-08 21:43:23 +0200

    Server e Client, primo controllo via netcat (Nmap).

--- src/main/java/it/polimi/ingsw/ClientMain.java
+++ src/main/java/it/polimi/ingsw/ClientMain.java
@@ -1,4 +1,13 @@
 package it.polimi.ingsw;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ClientMain {
+    public static void main(String[] args) {
+        System.out.println("Client started!");
+    }
 }
--- src/main/java/it/polimi/ingsw/ServerMain.java
+++ src/main/java/it/polimi/ingsw/ServerMain.java
@@ -1,15 +1,44 @@
 package it.polimi.ingsw;
 import java.net.Socket;
 import java.net.ServerSocket;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ServerMain {
+
     static int portNumber = 1234;
     public static void main(String[] args) throws IOException {
+
         System.out.println("Server started!");
         ServerSocket serverSocket = new ServerSocket(portNumber);
-        Socket clientSocket = serverSocket.accept();
+        Socket clientSocket= new Socket();
+
+        System.out.println("Accepting..");
+        try {
+            clientSocket = serverSocket.accept();
+            System.out.println("Accepted");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        try {
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String s = "";
+        try {
+            while ((s = in.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
