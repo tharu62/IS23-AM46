f9fa8c06c0207123cb64a610c5bba5a17c9ba7ab
LorenzoCodega <lorenzo.codega@mail.polimi.it>
2023-04-16 15:51:58 +0200

    Updated the method SetCardLogic(); added the attribute alreadyTakenLogic

--- src/main/java/it/polimi/ingsw/MODEL/P_CARD_LOGIC_GENERATOR.java
+++ src/main/java/it/polimi/ingsw/MODEL/P_CARD_LOGIC_GENERATOR.java
@@ -1,62 +1,32 @@
 package it.polimi.ingsw.MODEL;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class P_CARD_LOGIC_GENERATOR {
     P_CARD_LOGIC temp;
     Random rand = new Random();
     int upperbound = 12;
-    int i = rand.nextInt(upperbound);
+    List<Integer> alreadyTakenLogic = new ArrayList<>();
     public P_CARD_LOGIC SetCardLogic() {
-        if (i==1) {
-            this.temp = new P_CARD_LOGIC_1();
-            return temp;
-        }
-        if (i==2) {
-            this.temp = new P_CARD_LOGIC_2();
-            return temp;
-        }
-        if (i==3) {
-            this.temp = new P_CARD_LOGIC_3();
-            return temp;
-        }
-        if (i==4) {
-            this.temp = new P_CARD_LOGIC_4();
-            return temp;
-        }
-        if (i==5) {
-            this.temp = new P_CARD_LOGIC_5();
-            return temp;
-        }
-        if (i==6) {
-            this.temp = new P_CARD_LOGIC_6();
-            return temp;
-        }
-        if (i==7) {
-            this.temp = new P_CARD_LOGIC_7();
-            return temp;
-        }
-        if (i==8) {
-            this.temp = new P_CARD_LOGIC_8();
-            return temp;
-        }
-        if (i==9) {
-            this.temp = new P_CARD_LOGIC_9();
-            return temp;
-        }
-        if (i==10) {
-            this.temp = new P_CARD_LOGIC_10();
-            return temp;
-         }
-        if (i==11) {
-            this.temp = new P_CARD_LOGIC_11();
-            return temp;
-         }
-        if (i==12) {
-            this.temp = new P_CARD_LOGIC_12();
-            return temp;
-        }
-
-        return new P_CARD_LOGIC_12();
+        int i;
+        do {
+            i = rand.nextInt(upperbound);
+        } while (alreadyTakenLogic.contains(i));
+        alreadyTakenLogic.add(i);
+        if (i==0) this.temp = new P_CARD_LOGIC_1();
+        if (i==1) this.temp = new P_CARD_LOGIC_2();
+        if (i==2) this.temp = new P_CARD_LOGIC_3();
+        if (i==3) this.temp = new P_CARD_LOGIC_4();
+        if (i==4) this.temp = new P_CARD_LOGIC_5();
+        if (i==5) this.temp = new P_CARD_LOGIC_6();
+        if (i==6) this.temp = new P_CARD_LOGIC_7();
+        if (i==7) this.temp = new P_CARD_LOGIC_8();
+        if (i==8) this.temp = new P_CARD_LOGIC_9();
+        if (i==9) this.temp = new P_CARD_LOGIC_10();
+        if (i==10) this.temp = new P_CARD_LOGIC_11();
+        if (i==11) this.temp = new P_CARD_LOGIC_12();
+        return temp;
     }
 }
