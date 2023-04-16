d1d97d95c2098ab4be8a190ea745438a5bd0b060
LorenzoCodega <lorenzo.codega@mail.polimi.it>
2023-04-16 15:18:59 +0200

    Updated the method setItemList()

--- src/main/java/it/polimi/ingsw/MODEL/BAG.java
+++ src/main/java/it/polimi/ingsw/MODEL/BAG.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 public class BAG {
-    List<item> itemList= new ArrayList<item>(132);
+    List<item> itemList= new ArrayList<>(132);
     int CATS=0;
     int BOOKS=0;
     int GAMES=0;
@@ -14,75 +14,76 @@ public class BAG {
     int PLANTS=0;
 
     public void setItemList(){
-        for(int j=0; j<133;j++) {
-            Random rand = new Random();
-            int upperbound = 7;
-            int i = rand.nextInt(upperbound);
+        Random rand = new Random();
+        int upperbound = 7, i;
+        for(int j=0; j<132;j++) {
+            do {
+               i = rand.nextInt(upperbound);
+            } while (i == 0);
             int l=0;
 
             while(l==0){
-            switch (i){
-                case(1):{
-                    if(this.CATS<23){
-                        this.itemList.add(item.CATS);
-                        l++;
-                    }
-                    else{
-                        i++;
-                        continue;
-                    }
-
-                }
-                case(2):{
-                    if(this.GAMES<23){
-                        this.itemList.add(item.GAMES);
-                        l++;
-                    }
-                    else {
-                        i++;
-                        continue;
-                    }
-                }
-                case(3):{
-                    if(this.BOOKS<23){
-                        this.itemList.add(item.BOOKS);
-                        l++;
-                    }
-                    else {
-                        i++;
-                        continue;
+                switch (i) {
+                    case(1):
+                        if(this.CATS < 22){
+                            this.itemList.add(item.CATS);
+                            this.CATS++;
+                            l++;
+                            break;
+                        } else {
+                            i++;
+                            continue;
+                        }
+                    case(2):
+                        if(this.GAMES < 22){
+                            this.itemList.add(item.GAMES);
+                            this.GAMES++;
+                            l++;
+                            break;
+                        } else {
+                            i++;
+                            continue;
+                        }
+                    case(3):
+                        if(this.BOOKS < 22){
+                            this.itemList.add(item.BOOKS);
+                            this.BOOKS++;
+                            l++;
+                            break;
+                        } else {
+                            i++;
+                            continue;
+                        }
+                    case(4):
+                        if(this.FRAMES < 22){
+                            this.itemList.add(item.FRAMES);
+                            this.FRAMES++;
+                            l++;
+                            break;
+                        } else {
+                            i++;
+                            continue;
+                        }
+                    case(5):
+                        if(this.TROPHIES < 22){
+                            this.itemList.add(item.TROPHIES);
+                            this.TROPHIES++;
+                            l++;
+                            break;
+                        } else {
+                            i++;
+                            continue;
+                        }
+                    case(6):
+                        if(this.PLANTS < 22){
+                            this.itemList.add(item.PLANTS);
+                            this.PLANTS++;
+                            l++;
+                            break;
+                        } else {
+                            i = 1;
                         }
                 }
-                case(4):{
-                    if(this.FRAMES<23){
-                        this.itemList.add(item.FRAMES);
-                        l++;
-                    }
-                    else {
-                        i++;
-                        continue;
-                    }
-                }
-                case(5):{
-                    if(this.TROPHIES<23){
-                        this.itemList.add(item.TROPHIES);
-                        l++;
-                    }
-                    else{
-                        i++;
-                        continue;
-                    }
-                }
-                case(6):{
-                    if(this.PLANTS<23){
-                        this.itemList.add(item.PLANTS);
-                        l++;
-                    }
-                    else{
-                        i=1;
-                    }
-                }
-            }
             }
         }
     }
