25257a821862d1969c37ecdaace5a439d3d477ef
tharu62 <deshantharindu.edirisinghe@mail.polimi.it>
2023-06-07 12:26:53 +0200

    aggiornato il pom

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -2,10 +2,8 @@ package it.polimi.ingsw;
 
 public class App 
 {
-
     public static void main( String[] args ) throws Exception {
         SetUpper setUpper = new SetUpper();
         setUpper.run();
     }
-
 }
