e515c7326ddbdbcaa389fb006298916b62049cdf
AndreaaDalbaa <andrea.dalba@mail.polimi.it>
2023-06-07 13:38:46 +0200

    Creazione e collegamento delle scene di start e login
    creazione con scene builder dello start e del login sia per il primo giocatore che per i giocatori rimanenti e collegamento con il controller

--- src/main/java/it/polimi/ingsw/CONTROLLER_SCENE/Controller1.java
+++ src/main/java/it/polimi/ingsw/CONTROLLER_SCENE/Controller1.java
@@ -4,20 +4,43 @@ package it.polimi.ingsw.CONTROLLER_SCENE;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.EventObject;
+
+
 
 
 
 public class Controller1
 {
-    @FXML
-    ImageView myImageView;
-    Button myButton;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    private EventObject event;
 
-    public void startgame(ActionEvent actionEvent)
-    {
 
+    public void helloscene(ActionEvent actionEvent) throws IOException {
+        root= FXMLLoader.load(getClass().getResource("HelloScene0.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+    public void loginScene(ActionEvent actionEvent) throws IOException {
+        root= FXMLLoader.load(getClass().getResource("LoginScene0.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
---
+++ src/main/java/it/polimi/ingsw/CONTROLLER_SCENE/Main.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.CONTROLLER_SCENE;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main
+{
+
+        public void start(Stage stage)
+        {
+            try
+            {
+                Parent root= FXMLLoader.load(getClass().getResource("HelloScene0.fxml"));
+                Scene scene= new Scene(root);
+                stage.getScene();
+                stage.show();
+
+            } catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
